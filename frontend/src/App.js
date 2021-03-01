4330160b058427b1e5b2837ed47fa4784af43799
kiruba48 <kirubaiez@gmail.com>
2021-03-01 12:02:48 +0000

    Navbar initial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div>
-      hello all
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>HEMPIRE</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
-
-
-
+export default App;
