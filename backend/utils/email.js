6902b6c1a4260625803a7e4c97fca0b01f8d44bc
kiruba48 <kirubaiez@gmail.com>
2021-03-24 14:27:00 +0000

    clean up

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -24,13 +24,7 @@ export class Email {
         },
       });
     }
-    // return nodemailer.createTransport({
-    //   service: 'SendGrid',
-    //   auth: {
-    //     user: process.env.SENDGRID_USERNAME,
-    //     pass: process.env.SENDGRID_PASSWORD,
-    //   },
-    // });
+
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
